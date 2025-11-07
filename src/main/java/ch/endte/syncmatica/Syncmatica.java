7615984dea242b0c723f0a910b9187a68b4caae7
Trirrin <2628883576@qq.com>
2025-11-07 17:08:27 +0800

    chore: update version to 0.3.9

--- src/main/java/ch/endte/syncmatica/Syncmatica.java
+++ src/main/java/ch/endte/syncmatica/Syncmatica.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 public class Syncmatica {
 
-    public static final String VERSION = "0.3.8";
+    public static final String VERSION = "0.3.9";
     public static final String MOD_ID = "syncmatica";
     public static final Identifier CLIENT_CONTEXT = new Identifier("syncmatica:client_context");
     public static final Identifier SERVER_CONTEXT = new Identifier("syncmatica:server_context");
