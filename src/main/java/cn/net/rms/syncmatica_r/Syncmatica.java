135529dcdfd94d5d4dc2f2c5be6ac842b8e37538
Trirrin <2628883576@qq.com>
2025-11-12 01:07:07 +0800

    chore: bump mod version to 0.3.12

--- src/main/java/cn/net/rms/syncmatica_r/Syncmatica.java
+++ src/main/java/cn/net/rms/syncmatica_r/Syncmatica.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 public class Syncmatica {
 
-    public static final String VERSION = "0.3.11";
+    public static final String VERSION = "0.3.12";
     public static final String MOD_ID = "syncmatica_r";
     public static final String LEGACY_MOD_ID = "syncmatica";
     public static final Identifier CLIENT_CONTEXT = new Identifier(MOD_ID + ":client_context");
