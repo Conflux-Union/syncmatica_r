7c457052d23b60fee2c8455d3ee2353250bf29ba
Trirrin <2628883576@qq.com>
2025-11-14 01:03:24 +0800

    fix(client): update toast notification method for Minecraft versions
    Update toast notification method to support both MC >= 12001 and older versions, using conditional compilation.

--- src/main/java/cn/net/rms/syncmatica_r/client/SyncmaticaClient.java
+++ src/main/java/cn/net/rms/syncmatica_r/client/SyncmaticaClient.java
@@ -52,7 +52,11 @@ public class SyncmaticaClient implements ClientModInitializer {
         title = new TranslatableText("syncmatica_r.update.toast.title");
         description = new TranslatableText("syncmatica_r.update.toast.description", localVersion, remoteVersion);
         //#endif
-        SystemToast.add(client.getToastManager(), SystemToast.Type.TUTORIAL_HINT, title, description);
+        //#if MC >= 12001
+        //$$ SystemToast.add(client.getToastManager(), SystemToast.Type.TUTORIAL_HINT, title, description);
+        //#else
+        client.getToastManager().add(SystemToast.create(client, SystemToast.Type.TUTORIAL_HINT, title, description));
+        //#endif
         checker.markNotified();
     }
 }
